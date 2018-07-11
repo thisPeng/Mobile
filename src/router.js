4e610fde4ccfe6d0b21b01129a21a9bc5e50a90c
LANLANLAN960427 <1435840703@qq.com>
2018-07-11 15:45:07 +0800

    taskDemo

--- src/router.js
+++ src/router.js
@@ -10,75 +10,95 @@ import Users from "./views/Users/Users";
 import usersPwd from "./views/Users/usersPwd";
 import TaskWait from "./views/TaskWait/TaskWait";
 import usersInfo from "./views/Users/usersInfo";
-
+import TaskDemo from "./views/TaskWait/TaskDemo";
 Vue.use(Router);
 
 export default new Router({
   // mode: "history",
-  routes: [{
-    path: "/login",
-    name: "login",
-    component: Login
-  }, {
-    path: "/",
-    redirect: "/index",
-    component: Home,
-    children: [{
-      path: "/index",
-      name: "index",
-      component: Index,
-      meta: {
-        title: "首页" // 页面标题
-      }
-    }, {
-      path: "/taskWait",
-      name: "taskWait",
-      component: TaskWait,
-      meta: {
-        title: "我的待办" // 页面标题
-      }
-    }, {
-      path: "/message",
-      name: "message",
-      component: Message,
-      meta: {
-        title: "我的消息" // 页面标题
-      }
-    }, {
-      path: "/apply",
-      name: "apply",
-      component: Apply,
-      meta: {
-        title: "应用" // 页面标题
-      }
-    }, {
-      path: "/contact",
-      name: "contact",
-      component: Contact,
-      meta: {
-        title: "通讯录" // 页面标题
-      }
-    }, {
-      path: "/users",
-      name: "users",
-      component: Users,
-      meta: {
-        title: "用户中心" // 页面标题
-      }
-    }, {
-      path: "/users/usersPwd",
-      name: "usersPwd",
-      component: usersPwd,
-      meta: {
-        title: "修改密码" // 页面标题
-      }
-    }, {
-      path: "/users/usersInfo",
-      name: "usersInfo",
-      component: usersInfo,
-      meta: {
-        title: "个人信息" // 页面标题
-      }
-    }]
-  }]
+  routes: [
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/",
+      redirect: "/index",
+      component: Home,
+      children: [
+        {
+          path: "/index",
+          name: "index",
+          component: Index,
+          meta: {
+            title: "首页" // 页面标题
+          }
+        },
+        {
+          path: "/taskWait",
+          name: "taskWait",
+          component: TaskWait,
+          meta: {
+            title: "我的待办" // 页面标题
+          }
+        },
+        {
+          path: "/taskWait/taskDemo",
+          name: "taskDemo",
+          component: TaskDemo,
+          meta: {
+            title: "采购单移动运转" // 页面标题
+          }
+        },
+        {
+          path: "/message",
+          name: "message",
+          component: Message,
+          meta: {
+            title: "我的消息" // 页面标题
+          }
+        },
+        {
+          path: "/apply",
+          name: "apply",
+          component: Apply,
+          meta: {
+            title: "应用" // 页面标题
+          }
+        },
+        {
+          path: "/contact",
+          name: "contact",
+          component: Contact,
+          meta: {
+            title: "通讯录" // 页面标题
+          }
+        },
+        {
+          path: "/users",
+          name: "users",
+          component: Users,
+          meta: {
+            title: "用户中心" // 页面标题
+          }
+        },
+        {
+          path: "/users/usersPwd",
+          name: "usersPwd",
+          component: usersPwd,
+          meta: {
+            title: "修改密码" // 页面标题
+          }
+        },
+        {
+          path: "/users/usersInfo",
+          name: "usersInfo",
+          component: usersInfo,
+          meta: {
+            title: "个人信息" // 页面标题
+          }
+        }
+      ]
+    }
+  ]
 });
