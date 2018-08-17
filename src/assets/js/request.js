5a61deb21e4a91f434a10ce9550101fc019839ab
DJ <ddja@qq.com>
2018-08-17 16:26:17 +0800

    this

--- src/assets/js/request.js
+++ src/assets/js/request.js
@@ -8,9 +8,7 @@ import axios from "axios";
 // 根据环境设置请求服务器地址
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production" ?
-  getRootPath() :
-  "http://192.168.2.254/SupplyChain/";
-// "http://localhost:8088/";
+  getRootPath() : "http://localhost:8088/";
 // 响应时间
 axios.defaults.timeout = 10000;
 // `withCredentails`选项表明了是否是跨域请求
