f59bfdd7a6fa4e92ffbf0dfa6edb6f7c3a0b21db
DJ <ddja@qq.com>
2018-09-06 18:40:43 +0800

    annex

--- src/views/Transaction/MaterialQuotation/Tranrial.vue
+++ src/views/Transaction/MaterialQuotation/Tranrial.vue
@@ -2,21 +2,21 @@
   <!--报价单编辑 -->
   <div class="tranrial">
     <div class="title-delivery">发货单</div>
-    <van-cell-group v-for="(item,index) in list" :key="index">
-      <van-field v-model="item[2]" label="单据编号:" disabled />
-      <van-field v-model="item[33]" label="工程名称:" disabled />
-      <van-field v-model="item[5]" label="联系人:" disabled />
-      <van-field v-model="item[6]" label="联系电话:" disabled />
-      <van-field v-model="item[34]" label="工程地址:" disabled />
-      <van-field v-model="item[9]" label="订单数量:" disabled />
-      <van-field v-model="item[10]" label="订单金额:" disabled />
-      <van-field v-model="item[39]" label="订单状态:" disabled />
-      <van-field v-model="item[17]" label="订货有效期:" disabled />
-      <van-field v-model="item[18]" label="业务员:" disabled />
-      <van-field v-model="item[25]" label="创建日期:" disabled />
-      <van-field v-model="item[21]" label="备注:" disabled type="textarea" />
-      <van-cell title="报价单明细" is-link value="详情" @click="jumpPage(item)" />
-      <van-cell title="报价单附件" is-link value="详情" />
+    <van-cell-group>
+      <van-field v-model="info[2]" label="单据编号:" disabled />
+      <van-field v-model="info[33]" label="工程名称:" disabled />
+      <van-field v-model="info[5]" label="联系人:" disabled />
+      <van-field v-model="info[6]" label="联系电话:" disabled />
+      <van-field v-model="info[34]" label="工程地址:" disabled />
+      <van-field v-model="info[9]" label="订单数量:" disabled />
+      <van-field v-model="info[10]" label="订单金额:" disabled />
+      <van-field v-model="info[39]" label="订单状态:" disabled />
+      <van-field v-model="info[17]" label="订货有效期:" disabled />
+      <van-field v-model="info[18]" label="业务员:" disabled />
+      <van-field v-model="info[25]" label="创建日期:" disabled />
+      <van-field v-model="info[21]" label="备注:" disabled type="textarea" />
+      <van-cell title="报价单明细" is-link value="详情" @click="jumpPage(info)" />
+      <van-cell title="报价单附件" is-link value="详情" @click="jumpPage('tranrialAnnex')" />
     </van-cell-group>
     <div class="con-button">
       <van-button type="default">合同编辑</van-button>
@@ -32,21 +32,18 @@ import { offer } from "./../../../assets/js/api.js";
 export default {
   data() {
     return {
-      list: []
+      info: []
     };
   },
   computed,
   methods: {
     getOffer() {
       offer.getTranrial(this.confirmParams[0]).then(res => {
-        // console.log(res);
         if (res && res.status === 1) {
           const sp = res.text.split("[[");
-          // console.log(sp);
           const csp = sp[1].split(";");
-          //  console.log(csp);
-          this.list = eval("[[" + csp[0]);
-          //  console.log(this.list);
+          this.info = eval("[[" + csp[0])[0];
+          //  console.log(this.info);
         }
       });
     },
@@ -80,8 +77,8 @@ export default {
     pageInit() {
       this.getOffer();
     },
-    jumpPage(item) {
-      this.$store.commit("confirmParams", item);
+    jumpPage(info) {
+      this.$store.commit("confirmParams", info);
       this.$router.push({
         name: "tranrialDetail"
       });
